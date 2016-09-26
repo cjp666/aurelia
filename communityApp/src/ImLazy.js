906aeaf951efb37c867d393c567a7345fc270ffb
cjp666 <chris.j.podmore@gmail.com>
2016-09-26 15:35:41 +0100

    DI Lazy.of(T)

---
+++ communityApp/src/ImLazy.js
@@ -0,0 +1,11 @@
+import {log} from "log";
+
+export class ImLazy {
+    constructor() {
+        log.info("ImLazy constructor");
+    }
+
+    doStuff() {
+        log.info("ImLazy doStuff");
+    }
+}
--- communityApp/src/events.js
+++ communityApp/src/events.js
@@ -1,14 +1,22 @@
-import {inject} from 'aurelia-framework';
+import {inject, Lazy} from 'aurelia-framework';
+import {ImLazy} from "ImLazy";
+import {log} from "log";
 // import {DataCache} from "DataCache";
 
-@inject("Cache")
+@inject("Cache", Lazy.of(ImLazy))
 export class Events {
-    constructor(dataCache) {
+    constructor(dataCache, lazyOfImLazy) {
         this.events = [
-            { id:1, title: "Aurelia Fundamentals" },
-            { id:2, title: "Data-Centric SPAs with BreezeJS" }
+            { id: 1, title: "Aurelia Fundamentals" },
+            { id: 2, title: "Data-Centric SPAs with BreezeJS" }
         ];
         this.cache = dataCache;
         this.cache.data.push('a');
+        this.lazyOfImLazy = lazyOfImLazy;
+    }
+
+    createAndUseLazy(){
+        log.info("about to use lazy");
+        this.lazyOfImLazy().doStuff();
     }
 }
