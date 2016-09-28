6f70cca9464759edc96ad7fa2865d8992f86dd1f
cjp666 <chris.j.podmore@gmail.com>
2016-09-28 09:12:10 +0100

    tidy up src folder and change navigation

--- communityApp/src/shell.js
+++ communityApp/src/shell.js
@@ -1,3 +1,13 @@
 export class Shell {
+    configureRouter(config, router) {
+        this.router = router;
 
+        config.title = "Chirs's community group";
+
+        config.map([
+            { route: ["", "events", "abc"], moduleId: "events/events", name: "Events", title: "Events", nav: true },
+            { route: "jobs", moduleId: "jobs/jobs", title: "Jobs", nav: true },
+            { route: "discussion", moduleId: "discussion/discussion", title: "Discussion", nav: true }
+        ]);
+    }
 }
