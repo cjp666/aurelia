d595da4cb896ff6e3c521991304e7d7e51a05eca
cjp666 <chris.j.podmore@gmail.com>
2016-09-29 16:08:23 +0100

    two router-view's

--- communityApp/src/shell.js
+++ communityApp/src/shell.js
@@ -5,10 +5,10 @@ export class Shell {
         config.title = "Chirs's community group";
 
         config.map([
-            { route: ["", "events", "abc"], moduleId: "events/events", name: "Events", title: "Events", nav: true },
-            { route: "jobs", moduleId: "jobs/jobs", title: "Jobs", nav: true },
-            { route: "discussion", moduleId: "discussion/discussion", title: "Discussion", nav: true },
-            { route: "eventDetail/:eventId", moduleId: "events/eventDetail", name: "eventDetail" }
+            { route: ["", "events", "abc"], viewPorts: { mainContent: { moduleId: "events/events" }, sideBar: { moduleId: "sideBar/sponsors" } }, name: "Events", title: "Events", nav: true },
+            { route: "jobs", viewPorts: { mainContent: { moduleId: "jobs/jobs" }, sideBar: { moduleId: "sideBar/sponsors" } }, title: "Jobs", nav: true },
+            { route: "discussion", viewPorts: { mainContent: { moduleId: "discussion/discussion" }, sideBar: { moduleId: "sideBar/ads" } } , title: "Discussion", nav: true },
+            { route: "eventDetail/:eventId", viewPorts: { mainContent: { moduleId: "events/eventDetail" }, sideBar: { moduleId: "sideBar/ads" } }, name: "eventDetail" }
         ]);
     }
 }
