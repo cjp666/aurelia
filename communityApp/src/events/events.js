3b16ace6dcb0198bd53b43b3a42f4ded54fbd1ed
cjp666 <chris.j.podmore@gmail.com>
2016-09-29 11:18:54 +0100

    route parameters and programmaticly route

--- communityApp/src/events/events.js
+++ communityApp/src/events/events.js
@@ -5,9 +5,38 @@ import {DataRepository} from "services/dataRepository";
 @inject(DataRepository, Router)
 export class Events {
     constructor(dataRepository, router) {
-        dataRepository.getEvents().then(events => {
-            this.events = events;
-            this.events.forEach(item => item.detailUrl = router.generate("eventDetail", { eventId: item.id }));
+        this.dataRepository = dataRepository;
+        this.router = router;
+    }
+
+    activate(params) {
+        this.dataRepository.getEvents().then(events => {
+            if (params.speaker || params.topic) {
+                var filteredResults = [];
+                events.forEach(item => {
+                    if (params.speaker &&
+                        item.speaker.toLowerCase()
+                            .indexOf(params.speaker.toLowerCase()) >= 0) {
+                        filteredResults.push(item);
+                    }
+                    if (params.topic &&
+                        item.title.toLowerCase()
+                            .indexOf(params.topic.toLowerCase()) >= 0) {
+                        filteredResults.push(item);
+                    }
+                });
+                this.events = filteredResults;
+            }
+            else {
+                this.events = events;
+            }
+            this.events.forEach(item => item.detailUrl =
+                this.router.generate('eventDetail', { eventId: item.id }));
         });
+    }
+
+    gotoDiscussion() {
+        // this.router.navigate('#/discussion');
+        this.router.navigateToRoute('eventDetail', { eventId: this.events[0].id });
     }
 }
