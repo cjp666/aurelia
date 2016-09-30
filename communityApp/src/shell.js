7b977aa817a8c87fecece5dc49ac0d77f14c9e4e
cjp666 <chris.j.podmore@gmail.com>
2016-09-30 10:09:46 +0100

    added a custom navigation pipeline step

--- communityApp/src/shell.js
+++ communityApp/src/shell.js
@@ -1,3 +1,5 @@
+import {log} from "log";
+
 export class Shell {
     configureRouter(config, router) {
         this.router = router;
@@ -6,11 +8,25 @@ export class Shell {
 
         config.options.pushState = true;
 
+        config.addPipelineStep("authorize", LogNextStep);
+        config.addPipelineStep("preActivate", LogNextStep);
+        config.addPipelineStep("preRender", LogNextStep);
+        config.addPipelineStep("postRender", LogNextStep);
+
         config.map([
             { route: ["", "events", "abc"], viewPorts: { mainContent: { moduleId: "events/events" }, sideBar: { moduleId: "sideBar/sponsors" } }, name: "Events", title: "Events", nav: true },
             { route: "jobs", viewPorts: { mainContent: { moduleId: "jobs/jobs" }, sideBar: { moduleId: "sideBar/sponsors" } }, title: "Jobs", nav: true },
             { route: "discussion", viewPorts: { mainContent: { moduleId: "discussion/discussion" }, sideBar: { moduleId: "sideBar/ads" } } , title: "Discussion", nav: true },
             { route: "eventDetail/:eventId", viewPorts: { mainContent: { moduleId: "events/eventDetail" }, sideBar: { moduleId: "sideBar/ads" } }, name: "eventDetail" }
         ]);
+    }
+}
+
+class LogNextStep {
+    run(navigationInstruction, next) {
+        return next().then(result => {
+            log.debug(JSON.stringify(result));
+            return result;
+        });
     }
 }
